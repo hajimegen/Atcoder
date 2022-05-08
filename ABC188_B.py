ca55a533d45899a1e2dd5d411634ce1534173304
hajimegen <tsuchihaji8701@yahoo.ne.jp>
2022-05-08 15:34:10 -0600

    Add files via upload

---
+++ ABC188_B.py
@@ -0,0 +1,10 @@
+N = int(input())
+A =list(map(int,input().split()))
+B =list(map(int,input().split()))
+x = 0
+for i in range (N):
+  x += A[i]*B[i]
+if x == 0:
+  print("Yes")
+else:
+  print("No")
